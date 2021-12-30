447b4da9613e4305e371970f581fb408a72c1893
chirag <chiragdale0@gmail.com>
2021-12-30 16:52:58 +0530

    initial commit

---
+++ userboot/src/main/java/com/carscan/userboot/UserBootApplication.java
@@ -0,0 +1,13 @@
+package com.carscan.userboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserBootApplication.class, args);
+    }
+
+}
---
+++ userboot/src/test/java/com/carscan/userboot/UserBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carscan.userboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
